3fc177ad2c1813e228d8163e742b39fa857cc4dd
umarishtiaq248 <umarishtiaq63@gmail.com>
2024-12-24 18:41:24 +0500

    Improve code and make classes for management

--- Files/data_storage_class.py
+++ Files/data_storage_class.py
@@ -31,71 +31,85 @@ class WeatherRecord:
                 )
 
     @classmethod
-    def required_objects_calculation(cls, single_date):
+    def retrieve_objects_by_date(cls, single_date, data_validator):
         user_required_objects = []
-        for single_object in cls.all_record_objects:
-            try:
-                if single_date == "Y":
-                    if single_date.year == single_object.pkt.year:
-                        user_required_objects.append(single_object)
-                else:
-                    if (single_date.year == single_object.pkt.year) and (
-                        single_date.month == single_object.pkt.month
-                    ):
-                        user_required_objects.append(single_object)
-            except AttributeError as e:
-                pass
-        return user_required_objects
-
-    # Store every line of file
-    @classmethod
-    def store_data(cls, single_line, heading):
-        record = single_line.split(",")
-        heading = heading.split(",")
-        record = [item.strip() for item in record]
-        heading = [item.strip() for item in heading]
-        if len(record) <= 1:
-            pass
+        start_date = max(obj.pkt for obj in cls.all_record_objects)
+        end_date = min(obj.pkt for obj in cls.all_record_objects)
+        if start_date >= single_date >= end_date:
+            for single_object in cls.all_record_objects:
+                try:
+                    if data_validator == "Y":
+                        if single_date.year == single_object.pkt.year:
+                            user_required_objects.append(single_object)
+                    else:
+                        if (single_date.year == single_object.pkt.year) and (
+                            single_date.month == single_object.pkt.month
+                        ):
+                            user_required_objects.append(single_object)
+                except AttributeError as e:
+                    pass
+            return user_required_objects
         else:
-            single_record = [None if item == "" else item for item in record]
-            temp_dic = dict(zip(heading, single_record))
-            temp_object = cls(**temp_dic)
-            cls.all_record_objects.append(temp_object)
+            return False
 
-    # Open every file to read the data
+    # Open every file to read the data and store the data in class as object
     @classmethod
-    def read_path(cls, single_file_path):
-        if os.path.isfile(single_file_path):
-            with open(single_file_path, "r") as report:
-                # Header line of every file
-                heading = report.readline()
-                for single_line in report:
-                    cls.store_data(single_line, heading)
+    def read_and_store_files(cls, all_file_path):
+        for single_file_path in all_file_path:
+            if os.path.isfile(single_file_path):
+                with open(single_file_path, "r") as report:
+                    # Header line of every file
+                    heading = report.readline().split(",")
+                    heading = [item.strip() for item in heading]
+                    for single_line in report:
+                        record = single_line.split(",")
+                        record = [item.strip() for item in record]
+                        if len(record) <= 1:
+                            pass
+                        else:
+                            single_record = [
+                                None if item == "" else item for item in record
+                            ]
+                            temp_object = cls(**dict(zip(heading, single_record)))
+                            cls.all_record_objects.append(temp_object)
 
     # Get folder path from user and make a list of abs path for all weather record file
-    @classmethod
-    def load_path(cls, relative_path):
+    @staticmethod
+    def load_path(relative_path):
         cwd = os.getcwd()
         abs_path = os.path.join(cwd, relative_path)
         files_list = os.listdir(abs_path)
+        all_file_path = []
         for file_name in files_list:
-            single_file_path = os.path.join(abs_path, file_name)
-            cls.read_path(single_file_path)
+            all_file_path.append(os.path.join(abs_path, file_name))
+        return all_file_path
 
 
 class ReportGenerator:
     @staticmethod
     def task1(selected_instances):
-        max_value_instance = max(selected_instances, key=lambda x: x.max_temperature)
+        max_value_instance = max(
+            (x for x in selected_instances if x.max_temperature is not None),
+            key=lambda x: x.max_temperature,
+            default=None,
+        )
         max_temperature = max_value_instance.max_temperature
         max_temperature_date = max_value_instance.pkt
         # for finding minimum temperature
-        min_value_instance = max(selected_instances, key=lambda x: x.min_temperature)
+        min_value_instance = max(
+            (x for x in selected_instances if x.min_temperature is not None),
+            key=lambda x: x.min_temperature,
+            default=None,
+        )
         min_temperature = min_value_instance.min_temperature
         min_temperature_date = min_value_instance.pkt
 
         # for finding maximum humidity
-        max_humidity_instance = max(selected_instances, key=lambda x: x.max_humidit)
+        max_humidity_instance = max(
+            (x for x in selected_instances if x.max_humidit is not None),
+            key=lambda x: x.max_humidit,
+            default=None,
+        )
         max_humidity = max_humidity_instance.max_humidit
         max_humidity_date = max_humidity_instance.pkt
         print(f"Highest:{max_temperature}C on {max_temperature_date}")
@@ -164,23 +178,42 @@ class ReportGenerator:
     @classmethod
     def report_caller(cls, *flags_and_dates):
         for i, single_value in enumerate(flags_and_dates, start=1):
-            flag = single_value[1]
             single_date = single_value[0]
-            req_instances = WeatherRecord.required_objects_calculation(single_date)
-            print("Report ", i)
-            if flag == "-e":
-                print("Task 1")
-                cls.task1(req_instances)
-            elif flag == "-a":
-                print("Task 2")
-                cls.task2(req_instances)
-            elif flag == "-c":
-                print("Task 3")
-                cls.task3(req_instances)
-                print("Task 4")
-                cls.task4(req_instances)
+            flag = single_value[1]
+            date_validator = single_value[2]
+            req_instances = WeatherRecord.retrieve_objects_by_date(
+                single_date, date_validator
+            )
+            if req_instances:
+                print("Report ", i)
+                if flag == "-e":
+                    if date_validator != "Y":
+                        print("Flag Invalid")
+                    else:
+                        print("Report for given year")
+                        cls.task1(req_instances)
+                elif flag == "-a":
+                    if date_validator != "YM":
+                        print("Flag Invalid")
+                    else:
+                        print("Report for given month")
+                        cls.task2(req_instances)
+                elif flag == "-c":
+                    if date_validator != "YM":
+                        print("Flag Invalid")
+                    else:
+                        print(
+                            "Separate bars for Max and Min Temperature of given month by days"
+                        )
+                        cls.task3(req_instances)
+                        print(
+                            "Single bar for Max and Min Temperature of given month by days"
+                        )
+                        cls.task4(req_instances)
+                else:
+                    print(f"No Flag found for report :{i}")
             else:
-                print(f"No Flag found for report :{i}")
+                print(f"{single_date.year} not found")
 
 
 class ArgumentParser:
@@ -218,3 +251,21 @@ class ArgumentParser:
                     ).date()  # Year and month
                     self.flags_and_dates.append((temp_date, "-n", "YM"))
         return self.path, *self.flags_and_dates
+
+
+class ApplicationManager:
+    @staticmethod
+    def parse_and_extract_args(user_arguments):
+        path, *flags_and_dates = ArgumentParser(
+            *user_arguments[1:]
+        ).argument_generator()
+        return path, flags_and_dates
+
+    @staticmethod
+    def load_read_store(path):
+        all_file_path = WeatherRecord.load_path(path)
+        WeatherRecord.read_and_store_files(all_file_path)
+
+    @staticmethod
+    def process_report(flag_and_path):
+        ReportGenerator.report_caller(*flag_and_path)
--- Files/main.py
+++ Files/main.py
@@ -1,14 +1,7 @@
 from data_storage_class import *
 import sys
 
-
-def user_input(relative_path, *flags_and_dates_list):
-    WeatherRecord.load_path(relative_path)
-    ReportGenerator.report_caller(*flags_and_dates_list)
-
-
 if __name__ == "__main__":
-    user_input_path, *flags_and_dates = ArgumentParser(
-        *sys.argv[1:]
-    ).argument_generator()
-    user_input(user_input_path, *flags_and_dates)
+    path, flag_and_date_list = ApplicationManager.parse_and_extract_args(sys.argv)
+    ApplicationManager.load_read_store(path)
+    ApplicationManager.process_report(flag_and_date_list)
